82d71643990c24753ef641a6c7615025436f36ec
Anwesh43 <anweshthecool0@gmail.com>
2017-09-14 23:53:15 +0530

    adding State

--- index.js
+++ index.js
@@ -3,22 +3,39 @@ class Ripple {
         this.x = x
         this.y = y
         this.r = r
+        this.state = new State()
     }
     draw(context) {
         context.save()
         context.translate(this.x,this.y)
         context.strokeStyle = '#9C27B0'
         context.lineWidth = this.r/12
-        context.globalAlpha = 1
+        context.globalAlpha = this.state.scale
         context.beginPath()
-        context.arc(0,0,this.r,0,2*Math.PI)
+        context.arc(0,0,this.r*this.state.scale,0,2*Math.PI)
         context.stroke()
         context.restore()
     }
     update() {
-
+        this.state.update()
     }
     stopUpdating() {
-
+        return this.state.stopUpdating()
+    }
+}
+class State {
+    constructor() {
+        this.scale = 0
+        this.deg = 0
+    }
+    update() {
+        this.deg += (2*Math.PI)/30
+        this.scale = Math.abs(Math.sin(this.deg))
+        if(this.deg > Math.PI) {
+            this.deg = 0
+        }
+    }
+    stopUpdating() {
+        return this.deg == 0
     }
 }
